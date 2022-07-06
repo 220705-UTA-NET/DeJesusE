d7c17c90a2f614b37d0def3c70636bdefb951130
unknown <dejesusellery@gmail.com>
2022-07-06 15:26:11 -0400

    Edited .gitignore and added a C# Project

---
+++ MyFirstDotnet/Project/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Console.WriteLine is from the system library, and allows us access to write a line of text to the console.
+// (___) refers to parameters. These say we are sending Console.WriteLine whatever is inside the ().
+// "" denote a string literal
+Console.WriteLine("Hello, World!");
