4bf5e4e37e010ba882ac1d62c3fbbfa45bc4cc3b
unknown <dejesusellery@gmail.com>
2022-07-06 16:09:46 -0400

    Added C# Notes folder

---
+++ C#/C#Notes.cs
@@ -0,0 +1,15 @@
+
+Console.WriteLine("Hello, World!"); // Writes to the console
+Console.Write("Text"); // Same as Console.WriteLine but doesn't automatically end the line.
+
+Console.ReadLine(); // Reads from the console
+
+//EVERYTHING IS AN OBJECT
+// A object is a real world entity with state and behaviour
+
+// Data Types
+int x = 5; // integer - (whole number) 
+float f = 5.5f; // float
+double z = 5.5; // double
+long l = 5; //long 
+short b = 5; //short
--- MyFirstDotnet/Project/Program.cs
+++ MyFirstDotnet/Project/Program.cs
@@ -4,4 +4,5 @@
 // Console.WriteLine is from the system library, and allows us access to write a line of text to the console.
 // (___) refers to parameters. These say we are sending Console.WriteLine whatever is inside the ().
 // "" denote a string literal
+
 Console.WriteLine("Hello, World!");
