fcc19cae2b5d26b5e95a68a3c975e5e8215d02e3
unknown <dejesusellery@gmail.com>
2022-07-07 10:07:56 -0400

    Added more notes regarding prim. data types

--- C#/C#Notes.cs
+++ C#/C#Notes.cs
@@ -7,9 +7,30 @@ Console.ReadLine(); // Reads from the console
 //EVERYTHING IS AN OBJECT
 // A object is a real world entity with state and behaviour
 
-// Data Types
-int x = 5; // integer - (whole number) 
-float f = 5.5f; // float
-double z = 5.5; // double
-long l = 5; //long 
-short b = 5; //short
+// Statically type - declare the type before compiling
+// Dynamically type - declare the type during compiling
+
+// Primitive Data Types
+bool b = true; // Boolean - true or false
+
+sbyte level = 23; // Signed Integral - (+/-) integer, size: 1 byte
+byte bite = 62; // Unsigned Integral - (+) integer, size: 1 byte
+
+short b = 5; //short - a small integer, size: 2 bytes
+ushort value = 12312; // Unsigned short - (+) small integer, size: 2 bytes
+
+int x = 5; // integer - (whole number), size: 4 bytes
+uint y = 65535; // Unsigned integer - a (+) whole number, size: 4 bytes
+
+
+long l = 5; // long - a large integer, size: 8 bytes
+ulong ul = 1151092; // Unsigned long -  a (+) large integer, size: 8 bytes
+
+float f = 5.5f; // float - a single-precision floating-point decimal, size: 4 bytes
+double z = 5.5; // double - a double-precision floating-point decimal, size 8 bytes
+
+// char - a single (unicode) character, size: 2 bytes 
+char c1 = 'x';
+char c2 = '\u0042';
+
+decimal gpa = 3.5m; // decimal - a decimal; has more precision and smaller range than float; suitable for monetary calculations; size: 16 bytes
